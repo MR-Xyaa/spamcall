064c4fab67261a431810ab60032f10376a1a8fd2
MR-Xyaa <102247436+MR-Xyaa@users.noreply.github.com>
2022-03-25 01:37:07 +0700

    Create spam.py

---
+++ module/spam.py
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+#install project
+figlet Sabar Install Bahan | lolcat
+sleep 2
+
+git clone https://github.com/MrVirusSpm-07/projects-spam-call
+
+figlet selesai Tod | lolcat
+sleep 1
+
+cd projects-spam-call
+sleep 1
+
+# tampilan
+clear
+toilet -f big -F gay SpamCall 
+echo "=============================================" | lolcat
+echo "[•] Author : MR-Xyaa                         "
+echo "[•] FB     : Xyaa Xyaa                         "
+echo "[•] GitHub : github.com/MR-Xyaa              "
+echo "=============================================" | lolcat
+echo
+echo "=> 3x Spam Tunggu 20menit                    "
+echo "=> Jangan Berlebihan                         "
+echo "=> Follow FB dan Github Gw yakk^^            "
+echo
+echo "=============================================" | lolcat
+
+python projects.py
+
