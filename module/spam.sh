14b75a377a17836c1c2c46d7e59d7ac157b1f5da
MR-Xyaa <102247436+MR-Xyaa@users.noreply.github.com>
2022-03-25 02:59:33 +0700

    Create spam.sh

---
+++ module/spam.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+#install project
+figlet Install Bahan | lolcat
+sleep 2
+
+pkg install python
+
+figlet selesai | lolcat
+sleep 1
+
+pip install requests
+sleep 1
+
+# tampilan
+clear
+toilet -f big -F gay SpamCall 
+echo "=============================================" | lolcat
+echo "[•] Author : MR-Xyaa                         "
+echo "[•] FB     : Xyaa Xyaa                         "
+echo "[•] GitHub : github.com/MR-Xyaa              "
+echo "=============================================" | lolcat
+echo
+echo "=> 3x Spam Tunggu 20menit                    "
+echo "=> Jangan Berlebihan                         "
+echo "=> Follow FB dan Github Gw yakk^^            "
+echo
+echo "=============================================" | lolcat
+
+
+
